0515b126b236021f277f1b7f2ab984c47fa5434a
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2024-03-09 20:45:22 -0300

    infra: adjusting k8s settings

---
+++ kubernetes/up.ps1
@@ -0,0 +1,14 @@
+kubectl delete --all pods --namespace=default;`
+kubectl delete --all services --namespace=default;`
+
+
+#kubectl apply -f .\pod-database.yaml ;`
+kubectl apply -f .\cluster-ip-database.yaml ;`
+kubectl apply -f .\pod-services.yaml ;`
+kubectl apply -f .\cluster-ip-services.yaml ;`
+kubectl apply -f .\pod-auth-api.yaml ;`
+kubectl apply -f .\pod-core-api.yaml ;`
+kubectl apply -f .\cluster-ip-apis.yaml ;`
+kubectl apply -f .\pod-notification.yaml ;`
+kubectl apply -f .\cluster-ip-notification.yaml; `
+kubectl apply -f .\pod-web.yaml ;
