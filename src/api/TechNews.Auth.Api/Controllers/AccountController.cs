e45cc19ecb1ed0c740245233b203a8fa43f8e6eb
Igor Moura <igor.moura254@hotmail.com>
2024-03-13 21:34:31 -0300

    todo: must have the issuer url of the JWT in a environment variable

--- src/api/TechNews.Auth.Api/Controllers/AccountController.cs
+++ src/api/TechNews.Auth.Api/Controllers/AccountController.cs
@@ -127,7 +127,7 @@ public class AccountController : ControllerBase
 
         var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
         {
-            Issuer = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}",
+            Issuer = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}", // TODO: Colocar em um environment variable
             Subject = claims,
             Expires = DateTime.UtcNow.AddMinutes(EnvironmentVariables.TokenExpirationInMinutes),
             TokenType = tokenType,
