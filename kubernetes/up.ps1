4000066ff76d3382aad9c823b18da85e2a2764b0
Igor Moura <igor.moura254@hotmail.com>
2024-03-15 23:11:30 -0300

    infra(kubernetes): trying deployment file

--- kubernetes/up.ps1
+++ kubernetes/up.ps1
@@ -1,5 +1,6 @@
 kubectl delete --all pods --namespace=default;`
 kubectl delete --all services --namespace=default;`
+kubectl delete --all deployments --namespace=default;`
 
 
 kubectl apply -f .\pod-database.yaml ;`
@@ -7,6 +8,7 @@ kubectl apply -f .\cluster-ip-database.yaml ;`
 kubectl apply -f .\pod-services.yaml ;`
 kubectl apply -f .\cluster-ip-services.yaml ;`
 kubectl apply -f .\pod-auth-api.yaml ;`
+#kubectl apply -f .\deployment-core-api.yaml ;`
 kubectl apply -f .\pod-core-api.yaml ;`
 kubectl apply -f .\cluster-ip-apis.yaml ;`
 kubectl apply -f .\pod-notification.yaml ;`
